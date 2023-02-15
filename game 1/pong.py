e40735bea24e4900da18d93f68e7133e6b40e9c8
ramyozi <ramzibenmansour27@gmail.com>
2023-02-16 00:37:22 +0100

    Code updated + comments added

--- game 1/pong.py
+++ game 1/pong.py
@@ -1,132 +1,147 @@
 import turtle
-import winsound
-#import random
 
+import pygame
+import winsound
+
+# Initialisation de pygame et de la musique
+pygame.init()
+pygame.mixer.init()
+
+audio_file = r"C:\Users\ramzi\Desktop\aaaa.mp3"
+
+# Initialisation de la fenêtre
 wn = turtle.Screen()
 wn.title("Pong by ramzi")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-#Score
 score_a = 0
 score_b = 0
 
-#Paddle A (left)
+# Initialisation des raquettes et de la balle
 paddle_a = turtle.Turtle()
-paddle_a.speed(0) #speed of animation
+paddle_a.speed(0)  # vitesse de l'animation
 paddle_a.shape("square")
 paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
-paddle_a.goto(-350,0)
+paddle_a.goto(-350, 0)
 
-#Paddle B (right)
 paddle_b = turtle.Turtle()
-paddle_b.speed(0) #speed of animation
+paddle_b.speed(0)  # vitesse de l'animation
 paddle_b.shape("square")
 paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
-paddle_b.goto(350,0)
+paddle_b.goto(350, 0)
 
-#Ball
 ball = turtle.Turtle()
-ball.speed(0) #speed of animation
+ball.speed(0)  # vitesse de l'animation
 ball.shape("circle")
 ball.color("white")
 ball.penup()
-ball.goto(0,0)
+ball.goto(0, 0)
 ball.dx = 0.08
 ball.dy = 0.08
 
-#pen
+# Initialisation du score
 pen = turtle.Turtle()
-pen.speed(0) #speed of animation
+pen.speed(0)
 pen.color("white")
 pen.penup()
 pen.hideturtle()
-pen.goto(0,260)
-pen.write("Player A: 0 Player B: 0", align="center",font=("Courier",21,"normal"))
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 21, "normal"))
 
-#function
+# Fonctions pour déplacer les raquettes
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
     paddle_a.sety(y)
+
 
 def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
     paddle_a.sety(y)
 
+
 def paddle_b_up():
     y = paddle_b.ycor()
     y += 20
     paddle_b.sety(y)
+
 
 def paddle_b_down():
     y = paddle_b.ycor()
     y -= 20
     paddle_b.sety(y)
 
+# Fonction pour jouer un son
+def play_sound(sound_file):
+    pygame.mixer.music.load(sound_file)
+    pygame.mixer.music.play()
 
-#keyboard binding
+# Assignation des touches pour déplacer les raquettes
 wn.listen()
 wn.onkeypress(paddle_a_up, "z")
 wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
-# Main game loop
+# Boucle principale du jeu
 while True:
     wn.update()
 
-
-    #moving the ball
+    # Mouvement de la balle
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    #border checking
-
-#a = random.randint(-3, 3)
+    # Gestion des collisions avec les bords supérieurs et inférieurs de la fenêtre
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
-        winsound.PlaySound("aaaa.mp3",winsound.SND_FILENAME + winsound.SND_ASYNC )
-#     + winsound.SND_ASYNC
+        winsound.PlaySound(audio_file, winsound.SND_FILENAME + winsound.SND_ASYNC)
+
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
-        winsound.PlaySound("aaaa.mp3", winsound.SND_FILENAME + winsound.SND_ASYNC )
+        winsound.PlaySound(audio_file, winsound.SND_FILENAME + winsound.SND_ASYNC)
 
+    # Gestion des collisions avec les bords droit et gauche de la fenetre  et mise à jour des scores
     if ball.xcor() > 390:
-        paddle_b.goto(350,0)
-        paddle_a.goto(-350,0)
-        ball.goto(0,0)
-        ball.dx *=(-1)
+        paddle_b.goto(350, 0)
+        paddle_a.goto(-350, 0)
+        ball.goto(0, 0)
+        ball.dx *= (-1)
         score_a += 1
         pen.clear()
-        pen.write("Player A: {} Player B: {}".format(score_a,score_b), align="center", font=("Courier", 21, "normal"))
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 21, "normal"))
 
     if ball.xcor() < -390:
-        paddle_b.goto(350,0)
-        paddle_a.goto(-350,0)
-        ball.goto(0,0)
-        ball.dx *=(-1)
+        paddle_b.goto(350, 0)
+        paddle_a.goto(-350, 0)
+        ball.goto(0, 0)
+        ball.dx *= (-1)
         score_b += 1
         pen.clear()
-        pen.write("Player A: {} Player B: {}".format(score_a,score_b), align="center", font=("Courier", 21, "normal"))
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 21, "normal"))
 
-
-
-    #ball and paddle collision
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40 ):
+    # Gestion des collisions avec les raquettes
+    if (340 < ball.xcor() < 350) and (
+            paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
-        winsound.PlaySound("aaaa.mp3", winsound.SND_FILENAME + winsound.SND_ASYNC )
+        winsound.PlaySound(audio_file, winsound.SND_FILENAME + winsound.SND_ASYNC)
 
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40 ):
+    if (-340 > ball.xcor() > -350) and (
+            paddle_a.ycor() + 40 > ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
-        winsound.PlaySound("aaaa.mp3", winsound.SND_FILENAME + winsound.SND_ASYNC )
+        winsound.PlaySound(audio_file, winsound.SND_FILENAME + winsound.SND_ASYNC)
+        
+turtle.mainloop()
+
+
+
